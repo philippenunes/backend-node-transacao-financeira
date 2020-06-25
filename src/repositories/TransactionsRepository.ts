6eb51e4c164137416b4aee096df5f02ac13d1bf2
Nunes <philippenunes@live.com>
2020-06-25 17:13:07 -0300

    finalizando

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,55 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  title: string,
+  value: number,
+  type: 'income' | 'outcome';
+}
+
+
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
   constructor() {
+
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions
+    .reduce((acc: Balance, transaction: Transaction) => {
+      switch (transaction.type) {
+        case "income":
+          acc.income += transaction.value;
+          break;
+        case "outcome":
+          acc.outcome += transaction.value;
+          break;
+        default:
+          break;
+      }
+      return acc;
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
